91927b24ee57f7db8d185f26687be9e65ad5b6f2
Alina Bondarenko <alinamoskovchuk18@gmail.com>
2025-08-26 15:18:44 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,12 @@
-# Write your code here
+$resourceGroup = "mate-azure-task-5"
+$allDisks = Get-AzDisk -ResourceGroupName $resourceGroup
+
+$unattachedDisks = $allDisks | Where-Object {
+    -not $_.ManagedBy
+}
+
+$jsonOutput = $unattachedDisks | ConvertTo-Json
+
+$jsonOutput | Out-File -FilePath ".\result.json"
+
+Write-Host "result.json"
